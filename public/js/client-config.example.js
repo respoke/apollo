e94df0dcaa250fc03887060521f063f210c6aec8
Jeff Parrish <jparrish@digium.com>
2014-10-21 15:45:29 -0700

    fix config

--- public/js/client-config.example.js
+++ public/js/client-config.example.js
@@ -1,8 +1,8 @@
-var jira = require('./plugins/jira');
 var emo = require('./plugins/emo');
 var marked = require('./plugins/marked');
+var mention = require('./plugins/mention');
+var jira = require('./plugins/jira');
 var renderFile = require('./plugins/render-file');
-
 
 var clientConfig = {};
 
@@ -18,7 +18,28 @@ var clientConfig = {};
 clientConfig.messageRenderingMiddleware = [
     // jira(clientConfig.jiraBaseWithSlash),
     emo,
-    marked
+    marked,
+    mention
+];
+
+/**
+ * Which emo macros should be available?
+ * http://fortawesome.github.io/Font-Awesome/icons
+ */
+clientConfig.emoMacros = [
+     { display: '<i class="fa fa-smile-o"></i>',     name: 'smile', _id: ':)' },
+     { display: '<i class="fa fa-frown-o"></i>',     name: 'frown', _id: ':(' },
+     { display: '<i class="fa fa-heart"></i>',       name: 'heart', _id: ':heart:' },
+     { display: '<i class="fa fa-thumbs-up"></i>',   name: 'thumbs up', _id: ':thumbs-up:' },
+     { display: '<i class="fa fa-thumbs-down"></i>', name: 'thumbs down', _id: ':thumbs-down:' },
+     { display: '<i class="fa fa-fire"></i>',        name: 'fire', _id: ':fire:' },
+     { display: '<i class="fa fa-magic"></i>',       name: 'magic', _id: ':magic:' },
+     { display: '<i class="fa fa-rocket"></i>',      name: 'rocket', _id: ':rocket:' },
+     { display: '<i class="fa fa-check"></i>',       name: 'check ok', _id: ':check:' },
+     { display: '<i class="fa fa-male"></i>',        name: 'male', _id: ':male:' },
+     { display: '<i class="fa fa-female"></i>',      name: 'female', _id: ':female:' },
+     { display: '<i class="fa fa-bolt"></i>',        name: 'lightning bolt', _id: ':bolt:' },
+     { display: '<i class="fa fa-beer"></i>',        name: 'beer', _id: ':beer:' }
 ];
 
 // An optional async function for rendering how files will display in the UI.
